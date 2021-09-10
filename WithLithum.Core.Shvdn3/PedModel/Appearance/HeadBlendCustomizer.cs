e167d9d6e17a4947d1173c7c501c2a1b58cfa601
WithLithum <RelaperCrystal@163.com>
2021-09-10 11:17:51 +0800

    :construction: Face feature customization for Head Blend customization

--- WithLithum.Core.Shvdn3/PedModel/Appearance/HeadBlendCustomizer.cs
+++ WithLithum.Core.Shvdn3/PedModel/Appearance/HeadBlendCustomizer.cs
@@ -11,13 +11,13 @@ namespace WithLithum.Core.Shvdn3.PedModel.Appearance
     /// <summary>
     /// Provides head blend customization to model <see cref="PedHash.FreemodeMale01"/> and <see cref="PedHash.FreemodeFemale01"/>.
     /// </summary>
-    public class HeadBlendCustomizator
+    public class HeadBlendCustomizer
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="HeadBlendCustomizator"/> class.
+        /// Initializes a new instance of the <see cref="HeadBlendCustomizer"/> class.
         /// </summary>
         /// <param name="ped">The ped to customize.</param>
-        public HeadBlendCustomizator(Ped ped)
+        public HeadBlendCustomizer(Ped ped)
         {
             if (ped == null) throw new ArgumentNullException(nameof(ped));
             if (!ped.Exists()) throw new InvalidPoolObjectException(ped);
@@ -29,5 +29,15 @@ namespace WithLithum.Core.Shvdn3.PedModel.Appearance
         /// Gets the ped of this instance.
         /// </summary>
         public Ped BasePed { get; }
+
+        /// <summary>
+        /// Gets an instance of <see cref="FaceFeature"/> based on the specified <see cref="FaceFeatureType"/>.
+        /// </summary>
+        /// <param name="type">The type of the face feature.</param>
+        /// <returns>An instance of <see cref="FaceFeature"/> which can be used to change the value of a specified face feature.</returns>
+        public FaceFeature this[FaceFeatureType type]
+        {
+            get => new FaceFeature(this, type);
+        }
     }
 }
