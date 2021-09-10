12e903b99c01e97c314ae7b4822c51a7f72f6d36
WithLithum <RelaperCrystal@163.com>
2021-09-11 07:32:07 +0800

    :construction: Head blend customization

--- WithLithum.Core.Shvdn3/PedModel/Appearance/HeadBlendCustomizer.cs
+++ WithLithum.Core.Shvdn3/PedModel/Appearance/HeadBlendCustomizer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GTA;
+using GTA.Native;
 using WithLithum.Core.Shvdn3.Exceptions;
 
 namespace WithLithum.Core.Shvdn3.PedModel.Appearance
@@ -31,13 +32,23 @@ namespace WithLithum.Core.Shvdn3.PedModel.Appearance
         public Ped BasePed { get; }
 
         /// <summary>
+        /// Gets an instance of <see cref="HeadBlend"/> which can be used to manipulate head blend data of this instance.
+        /// </summary>
+        /// <value>
+        /// An instance of <see cref="HeadBlend"/> which can be used to manipulate head blend data of this instance.
+        /// </value>
+        public HeadBlend Blend { get; set; }
+
+        /// <summary>
         /// Gets an instance of <see cref="FaceFeature"/> based on the specified <see cref="FaceFeatureType"/>.
         /// </summary>
         /// <param name="type">The type of the face feature.</param>
         /// <returns>An instance of <see cref="FaceFeature"/> which can be used to change the value of a specified face feature.</returns>
-        public FaceFeature this[FaceFeatureType type]
+        public FaceFeature this[FaceFeatureType type] => new FaceFeature(this, type);
+
+        internal void SetHeadBlendInternal(int shapeFirstID, int shapeSecondID, int shapeThirdID, int skinFirstID, int skinSecondID, int skinThirdID, float shapeMix, float skinMix, float thirdMix, bool isParent)
         {
-            get => new FaceFeature(this, type);
+            Function.Call(Hash.SET_PED_HEAD_BLEND_DATA, BasePed, shapeFirstID, shapeSecondID, shapeThirdID, skinFirstID, skinSecondID, skinThirdID, shapeMix, skinMix, thirdMix, isParent);
         }
     }
 }
---
+++ WithLithum.Core.Shvdn3/PedModel/Appearance/HeadBlendParent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Shvdn3.PedModel.Appearance
+{
+    /// <summary>
+    /// An enumeration of all possible parents in head blend configuration.
+    /// </summary>
+    /// <remarks>
+    /// <note type="important">This list may not be accurate, because this list is simply chronologically ordered instead of tested actually.<br />
+    /// For more information, see <see href="https://gtaforums.com/topic/858970-all-gtao-face-ids-pedset_ped_head_blend_data-explained">this topic</see> on GTAForums.</note>
+    /// </remarks>
+    /// <seealso cref="HeadBlend"/>
+    /// <seealso cref="HeadBlendCustomizer"/>
+    public enum HeadBlendParent
+    {
+        Benjamin = 1,
+        Daniel,
+        Joshua,
+        Noah,
+        Andrew,
+        Juan,
+        Alex,
+        Isaac,
+        Evan,
+        Ethan,
+        Vincent,
+        Angel,
+        Diego,
+        Adrian,
+        Gabriel,
+        Michael,
+        Santiago,
+        Kevin,
+        Louis,
+        Samuel,
+        Anthony,
+        Hannah,
+        Audrey,
+        Jasmine,
+        Giselle,
+        Amelia,
+        Isabella,
+        Zoe,
+        Ava,
+        Camila,
+        Violet,
+        Sophia,
+        Evelyn,
+        Nicole,
+        Ashley,
+        Grace,
+        Brianna,
+        Natalie,
+        Olivia,
+        Elizabeth,
+        Charlotte,
+        Emma,
+        Claude,
+        Niko,
+        John,
+        Misty
+    }
+}
