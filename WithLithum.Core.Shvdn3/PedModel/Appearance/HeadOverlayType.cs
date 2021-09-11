f9b2106ec060f6d7851e614344580db919962121
WithLithum <RelaperCrystal@163.com>
2021-09-11 08:26:38 +0800

    :construction: Head overlay customization

---
+++ WithLithum.Core.Shvdn3/PedModel/Appearance/HeadOverlayType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.PedModel.Appearance
+{
+    /// <summary>
+    /// An enumeration of all possible head overlay types.
+    /// </summary>
+    /// <seealso cref="HeadOverlay"/>
+    /// <seealso cref="HeadBlendCustomizer"/>
+    public enum HeadOverlayType
+    {
+        Blemishes,
+        FacialHair,
+        Eyebrows,
+        Ageing,
+        Makeup,
+        Blush,
+        Complexion,
+        SunDamage,
+        Lipstick,
+        Freckles,
+        ChestHair,
+        BodyBlemishes,
+        ExtraBodyBlemishes
+    }
+}
