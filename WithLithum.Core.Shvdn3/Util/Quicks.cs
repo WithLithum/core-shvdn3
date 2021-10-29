d139b53879fe83301149aeceb9ef62b9bb45b700
WithLithum <RelaperCrystal@163.com>
2021-10-29 15:16:10 +0800

    :sparkles: Add quick accesses

---
+++ WithLithum.Core.Shvdn3/Util/Quicks.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GTA;
+
+namespace WithLithum.Core.Util
+{
+    /// <summary>
+    /// Provide quick accesses. This class is meant to be imported using <c>using static</c>.
+    /// </summary>
+    public static class Quicks
+    {
+        /// <summary>
+        /// Gets the player character.
+        /// </summary>
+        /// <value>
+        /// The player character.
+        /// </value>
+        public static Ped PChara => Game.Player.Character;
+
+        /// <summary>
+        /// Gets the player's current vehicle.
+        /// </summary>
+        /// <value>
+        /// If player's in vehicle, player's current vehicle; otherwise, <c>null</c>.
+        /// </value>
+        public static Vehicle PVehicle => Game.Player.Character.CurrentVehicle;
+    }
+}
