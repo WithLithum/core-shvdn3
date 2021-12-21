7935008884cbb923da17be2d21f2234614cf6c92
WithLithum <RelaperCrystal@163.com>
2021-12-21 14:23:43 +0800

    :sparkles: Add more session related stuff

--- WithLithum.Core.Shvdn3/Engine/Session.cs
+++ WithLithum.Core.Shvdn3/Engine/Session.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using static WithLithum.Core.Util.NativeFunctions;
+using static WithLithum.Core.Util.Native.API;
 
 namespace WithLithum.Core.Engine
 {
@@ -21,5 +22,21 @@ namespace WithLithum.Core.Engine
         /// If in multiplayer, the number of players in the current session; otherwise, always <c>1</c>.
         /// </value>
         public static int PlayerCount => GetNumberOfPlayers();
+
+        /// <summary>
+        /// Gets a value indicating whether this client is in a solo session.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this session is a solo session; otherwise, <c>false</c>.
+        /// </value>
+        public static bool IsSolo => NetworkSessionIsSolo();
+
+        /// <summary>
+        /// Gets a value indicating whether this client is in a closed session.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this session is closed; otherwise, <c>false</c>.
+        /// </value>
+        public static bool IsClosed => NetworkSessionIsPrivate();
     }
 }
---
+++ WithLithum.Core.Shvdn3/Util/Native/API.Network.cs
@@ -0,0 +1,33 @@
+﻿using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Util.Native
+{
+    /// <summary>
+    /// Provides wrappers for native functions.
+    /// </summary>
+    public static partial class API
+    {
+        /// <summary>
+        /// Determines whether the current session is a solo session.
+        /// </summary>
+        /// <returns><c>true</c> if it is a solo session; otherwise, <c>false</c>.</returns>
+        public static bool NetworkSessionIsSolo()
+        {
+            return Function.Call<bool>(Hash.NETWORK_SESSION_IS_SOLO);
+        }
+
+        /// <summary>
+        /// Determines whether the current session is a closed session.
+        /// </summary>
+        /// <returns><c>true</c> if this session is closed; otherwise, <c>false</c>.</returns>
+        public static bool NetworkSessionIsPrivate()
+        {
+            return Function.Call<bool>(Hash.NETWORK_SESSION_IS_PRIVATE);
+        }
+    }
+}
