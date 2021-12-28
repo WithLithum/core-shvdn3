5ded3a799e9f247a94a60800fb77dc1ecf2c96c1
WithLithum <RelaperCrystal@163.com>
2021-12-28 18:02:26 +0800

    :sparkles: Pause-Menu related API

---
+++ WithLithum.Core.Shvdn3/UI/Hud/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WithLithum.Core.Util.Native;
+
+namespace WithLithum.Core.UI.Hud
+{
+    /// <summary>
+    /// Provides methods and properties to manipulate Pause Menu.
+    /// </summary>
+    public static class PauseMenu
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether the pause menu is open.
+        /// </summary>
+        /// <value>
+        /// <see langword="true"/> if the pause menu is open; otherwise, <see langword="false"/>.
+        /// </value>
+        public static bool IsOpen
+        {
+            get => API.IsPauseMenuActive();
+            set => API.SetPauseMenuActive(value);
+        }
+
+        /// <summary>
+        /// Gets the position of the cursor on the pause menu screen.
+        /// </summary>
+        /// <value>
+        /// If the pause menu is open, an instance of <see cref="PointF"/> representing
+        /// the cursor position; otherwise, returns 0.
+        /// </value>
+        public static PointF CursorPosition
+        {
+            get
+            {
+                var v3 = API.GetPauseMenuCursorPosition();
+                return new PointF(v3.X, v3.Y);
+            }
+        }
+    }
+}
---
+++ WithLithum.Core.Shvdn3/Util/Native/API.Hud.cs
@@ -0,0 +1,32 @@
+﻿using GTA.Math;
+using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Util.Native
+{
+    public static partial class API
+    {
+        /// <summary>
+        /// <br /><i>This is the wrapper for native function 0xB0034A223497FFCB.</i>
+        /// </summary>
+        public static bool IsPauseMenuActive() => Function.Call<bool>(Hash.IS_PAUSE_MENU_ACTIVE);
+
+        /// <summary>
+        /// <br /><i>This is the wrapper for native function 0xDF47FC56C71569CF.</i>
+        /// </summary>
+        public static void SetPauseMenuActive(bool toggle) => Function.Call(Hash.SET_PAUSE_MENU_ACTIVE, toggle);
+
+        /// <summary>
+        /// <br /><i>This is the wrapper for native function 0x5BFF36D6ED83E0AE.</i>
+        /// </summary>
+        /// <returns>If the pause menu is open, it will return a Vector3, Z is always 0. If the pause menu is closed, it will return Vector3.Zero</returns>
+        public static Vector3 GetPauseMenuCursorPosition()
+        {
+            return Function.Call<Vector3>((Hash)0x5BFF36D6ED83E0AE);
+        }
+    }
+}
