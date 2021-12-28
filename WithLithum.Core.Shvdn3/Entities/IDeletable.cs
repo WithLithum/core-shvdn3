1a6126cf9079a037d3d2d97c7af87460d7bbf60d
WithLithum <RelaperCrystal@163.com>
2021-12-28 18:37:35 +0800

    :sparkles: Add vehicle wrapper

---
+++ WithLithum.Core.Shvdn3/Entities/IDeletable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Entities
+{
+    /// <summary>
+    /// Represents an object that can be deleted.
+    /// </summary>
+    public interface IDeletable
+    {
+        /// <summary>
+        /// Deletes this instance from the game world immediately, and sets the handle of this instance to
+        /// <c>0</c>.
+        /// </summary>
+        /// <remarks>
+        /// <note type="warning">
+        /// Calls to an instance that has been deleted are invalid and will result in an exception.
+        /// </note>
+        /// <para>
+        /// Note that you must remove this instance from a collection first if the collection uses <see cref="object.Equals(object)"/>
+        /// for equality comparison, before deleting this instance.
+        /// </para>
+        /// </remarks>
+        void Delete();
+    }
+}
---
+++ WithLithum.Core.Shvdn3/Util/Native/API.Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Util.Native
+{
+    public static partial class API
+    {
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="vehicle"><i>No description provided.</i></param>
+        /// <returns>Returns 1000.0 if the function is unable to get the address of the specified vehicle or if it's not a vehicle. </returns>
+        public static float GetVehicleEngineHealth(int vehicle) => Function.Call<float>(Hash.GET_VEHICLE_ENGINE_HEALTH, vehicle);
+
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="vehicle"><i>No description provided.</i></param>
+        /// <param name="value"><i>No description provided.</i></param>
+        public static void SetVehicleEngineHealth(int vehicle, float value) => Function.Call(Hash.SET_VEHICLE_ENGINE_HEALTH, vehicle, value);
+
+        /// <summary>
+        /// Only ever used once in decompiled scripts: am_pi_menu: Returns true if the engine is on fire, or if the vehicle engine health is lesser than 0 and it has been on fire.
+        /// </summary>
+        /// <param name="vehicle">The vehicle to check.</param>
+        /// <returns>Returns true when the engine of your vehicle is on fire. Returns false if it's not on fire.</returns>
+        public static bool IsVehicleEngineOnFire(int vehicle) => Function.Call<bool>(Hash._IS_VEHICLE_ENGINE_ON_FIRE, vehicle);
+    }
+}
