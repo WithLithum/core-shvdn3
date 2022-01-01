2608342c278d67abdff8dabbf965135c411f63b8
WithLithum <RelaperCrystal@163.com>
2022-01-01 17:21:28 +0800

    :construction: Add thread util

---
+++ WithLithum.Core.Shvdn3/Engine/ThreadUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WithLithum.Core.Util.Native;
+
+namespace WithLithum.Core.Engine
+{
+    /// <summary>
+    /// Provides utilties to manipulate scripting.
+    /// </summary>
+    public static class ThreadUtil
+    {
+        /// <summary>
+        /// Determines whether the forced cleanup of the specified type has occured.
+        /// </summary>
+        /// <param name="flags">The type of the cleanup.</param>
+        /// <returns><see langword="true"/> if the specified cleanup occured; otherwise, <see langword="false"/>.</returns>
+        public static bool HasForcedCleanupOccured(int flags) => Api.HasForceCleanupOccurred(flags);
+    }
+}
---
+++ WithLithum.Core.Shvdn3/Util/Native/Api.Player.cs
@@ -0,0 +1,19 @@
+﻿using GTA.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithLithum.Core.Util.Native
+{
+    public static partial class Api
+    {
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="cleanupFlags"><i>No description provided.</i></param>
+        /// <returns><i>No description provided.</i></returns>
+        public static bool HasForceCleanupOccurred(int cleanupFlags) => Function.Call<bool>(Hash.HAS_FORCE_CLEANUP_OCCURRED, cleanupFlags);
+    }
+}
