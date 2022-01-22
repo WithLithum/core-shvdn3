dd4af48995e2b63870dcf28c546856443f25a930
WithLithum <RelaperCrystal@163.com>
2022-01-22 08:38:09 +0800

    :boom: Move PedModel to entities

--- WithLithum.Core.Shvdn3/Entities/PedModel/Appearance/HeadBlendParent.cs
+++ WithLithum.Core.Shvdn3/Entities/PedModel/Appearance/HeadBlendParent.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (C) WithLithum & contributors 2021-2022.
 // Licensed under LGPL-3.0-or-later license. See LICENSE for more info.
 
-namespace WithLithum.Core.PedModel.Appearance;
+namespace WithLithum.Core.Entities.PedModel.Appearance;
 
 using JetBrains.Annotations;
 
--- WithLithum.Core.Shvdn3/Entities/PedModel/PedExtensions.cs
+++ WithLithum.Core.Shvdn3/Entities/PedModel/PedExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (C) WithLithum & contributors 2021-2022.
 // Licensed under LGPL-3.0-or-later license. See LICENSE for more info.
 
-namespace WithLithum.Core.PedModel;
+namespace WithLithum.Core.Entities.PedModel;
 
 using GTA;
 using JetBrains.Annotations;
