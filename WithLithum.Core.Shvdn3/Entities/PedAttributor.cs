dcaa54b38f32516545da3a26eb02f474a4fbbe52
WithLithum <RelaperCrystal@163.com>
2022-02-05 15:27:41 +0800

    :sparkles: Ringtone for v3

--- WithLithum.Core.Shvdn3/Entities/PedAttributor.cs
+++ WithLithum.Core.Shvdn3/Entities/PedAttributor.cs
@@ -22,10 +22,32 @@ public class PedAttributor : EntityAttributor
     }
 
     /// <summary>
+    /// Gets a value indicating whether the ringtone of this instance is playing.
+    /// </summary>
+    public bool IsRingtonePlaying => Api.IsPedRingtonePlaying(this.RequiresValid().Handle);
+
+    /// <summary>
+    /// Plays the ringtone of this instance.
+    /// </summary>
+    /// <param name="ringtone">The ringtone to play. Usually <c>Remote_Ring</c>, or <c>Dial_and_Remote_Ring</c>.</param>
+    public void PlayRingtone(string ringtone)
+    {
+        Api.PlayPedRingtone(ringtone, RequiresValid().Handle, true);
+    }
+
+    /// <summary>
     /// Blows the head of this instance.
     /// </summary>
     public void BlowHead()
     {
         Api.ExplodePedHead(RequiresValid().Handle, (uint)WeaponHash.Pistol);
+    }
+
+    /// <summary>
+    /// Stops the ringtone being played on this instance.
+    /// </summary>
+    public void StopRingtone()
+    {
+        Api.StopPedRingtone(RequiresValid().Handle);
     }
 }
--- WithLithum.Core.Shvdn3/Util/Native/API.Audio.cs
+++ WithLithum.Core.Shvdn3/Util/Native/API.Audio.cs
@@ -7,14 +7,29 @@ using System.Threading.Tasks;
 
 namespace WithLithum.Core.Util.Native
 {
-    public static partial class API
+    public static partial class Api
     {
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="ringtoneName"><i>No description provided.</i></param>
+        /// <param name="ped"><i>No description provided.</i></param>
+        /// <param name="p2"><i>No description provided.</i></param>
         public static void PlayPedRingtone(string ringtoneName, int /* Ped */ ped, bool p2)
-            => Function.Call(Hash.PLAY_PED_RINGTONE, ped, p2);
+            => Function.Call(Hash.PLAY_PED_RINGTONE, ringtoneName, ped, p2);
 
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="ped"><i>No description provided.</i></param>
+        /// <returns><i>No description provided.</i></returns>
         public static bool IsPedRingtonePlaying(int /* Ped */ ped)
             => Function.Call<bool>(Hash.IS_PED_RINGTONE_PLAYING, ped);
 
+        /// <summary>
+        /// <i>No description provided.</i>
+        /// </summary>
+        /// <param name="ped"><i>No description provided.</i></param>
         public static void StopPedRingtone(int /* Ped */ ped)
             => Function.Call(Hash.STOP_PED_RINGTONE, ped);
     }
